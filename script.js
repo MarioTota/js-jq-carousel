2b51a67f488f15345d159142dd628ce879c5f858
Mario <mario.tota.mt@gmail.com>
2020-11-24 16:05:36 +0100

    commit

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
 
   $(".far.fa-caret-square-right").click(
     function () {
-
-
     if ($(".uno").hasClass("active") == true) {
       $(".uno").removeClass("active");
       $(".uno").next().addClass("active");
@@ -16,16 +14,27 @@ $(document).ready(function () {
       $(".tre").next().addClass("active");
      } else if ($(".quattro").hasClass("active") == true) {
       $(".quattro").removeClass("active");
-      $(".uno").next().addClass("active");
-    }
-
-      // $("img").removeClass("active");
-
-      // $("img:nth-of-type(1)").next().addClass("active");
-
-
+      $(".uno").addClass("active");
+      }
     }
   )
 
+  $(".far.fa-caret-square-left").click(
+    function () {
+    if ($(".uno").hasClass("active") == true) {
+      $(".uno").removeClass("active");
+      $(".quattro").addClass("active");
+    } else if ($(".quattro").hasClass("active") == true) {
+      $(".quattro").removeClass("active");
+      $(".quattro").prev().addClass("active");
+     } else if ($(".tre").hasClass("active") == true) {
+      $(".tre").removeClass("active");
+      $(".tre").prev().addClass("active");
+    } else if ($(".due").hasClass("active") == true) {
+      $(".due").removeClass("active");
+      $(".due").prev().addClass("active");
+      }
+    }
+  )
 
 });
