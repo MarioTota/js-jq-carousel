fec8a843ef0b0cf9f1852f28ab58156c424f87cc
Mario <mario.tota.mt@gmail.com>
2020-11-24 15:56:07 +0100

    commit

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
 
+
   $(".far.fa-caret-square-right").click(
-    function() {
-      $("img:nth-of-type(1)").removeClass("active");
-      $("img:nth-of-type(1)").next().addClass("active");
+    function () {
+
+
+    if ($(".uno").hasClass("active") == true) {
+      $(".uno").removeClass("active");
+      $(".uno").next().addClass("active");
+    } else if ($(".due").hasClass("active") == true) {
+      $(".due").removeClass("active");
+      $(".due").next().addClass("active");
+     } else if ($(".tre").hasClass("active") == true) {
+      $(".tre").removeClass("active");
+      $(".tre").next().addClass("active");
+     } else if ($(".quattro").hasClass("active") == true) {
+      $(".quattro").removeClass("active");
+      $(".uno").next().addClass("active");
+    }
+
+      // $("img").removeClass("active");
+
+      // $("img:nth-of-type(1)").next().addClass("active");
+
+
     }
   )
-
 
 
 });
