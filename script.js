998fa2b4f7545b8a3415fe0e08d800399c904a1a
Mario <mario.tota.mt@gmail.com>
2020-11-24 23:26:56 +0100

    commit

--- script.js
+++ script.js
@@ -15,37 +15,43 @@ $(document).ready(function () {
     }
   }
 
-  $(".far.fa-caret-square-right").click(
-    // function nextImg() {
-    //   if ($(".uno").hasClass("active") == true) {
-    //     $(".uno").removeClass("active");
-    //     $(".uno").next().addClass("active");
-    //   } else if ($(".due").hasClass("active") == true) {
-    //     $(".due").removeClass("active");
-    //     $(".due").next().addClass("active");
-    //   } else if ($(".tre").hasClass("active") == true) {
-    //     $(".tre").removeClass("active");
-    //     $(".tre").next().addClass("active");
-    //   } else if ($(".quattro").hasClass("active") == true) {
-    //     $(".quattro").removeClass("active");
-    //     $(".uno").addClass("active");
-    //   }
-    // }
-  )
+  function prevImg() {
+    if ($(".uno").hasClass("active") == true) {
+      $(".uno").removeClass("active");
+      $(".quattro").addClass("active");
+    } else if ($(".quattro").hasClass("active") == true) {
+      $(".quattro").removeClass("active");
+      $(".quattro").prev().addClass("active");
+    } else if ($(".tre").hasClass("active") == true) {
+      $(".tre").removeClass("active");
+      $(".tre").prev().addClass("active");
+    } else if ($(".due").hasClass("active") == true) {
+      $(".due").removeClass("active");
+      $(".due").prev().addClass("active");
+    }
+  }
 
-  $(document).keydown (
-    function() {
-      if (event.which == 39) {
-        nextImg();
+  $(".far.fa-caret-square-right").click(
+    function nextImg() {
+      if ($(".uno").hasClass("active") == true) {
+        $(".uno").removeClass("active");
+        $(".uno").next().addClass("active");
+      } else if ($(".due").hasClass("active") == true) {
+        $(".due").removeClass("active");
+        $(".due").next().addClass("active");
+      } else if ($(".tre").hasClass("active") == true) {
+        $(".tre").removeClass("active");
+        $(".tre").next().addClass("active");
+      } else if ($(".quattro").hasClass("active") == true) {
+        $(".quattro").removeClass("active");
+        $(".uno").addClass("active");
       }
     }
   )
 
 
-
-
   $(".far.fa-caret-square-left").click(
-    function () {
+    function prevImg() {
       if ($(".uno").hasClass("active") == true) {
         $(".uno").removeClass("active");
         $(".quattro").addClass("active");
@@ -62,5 +68,14 @@ $(document).ready(function () {
     }
   )
 
+  $(document).keydown (
+    function() {
+      if (event.which == 39) {
+        nextImg();
+      } else if (event.which == 37) {
+        prevImg();
+      }
+    }
+  )
 
 });
