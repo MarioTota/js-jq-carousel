bca89bfda812c926909f6fc289ed6941e2092145
Mario <mario.tota.mt@gmail.com>
2020-11-24 17:23:39 +0100

    commit

--- script.js
+++ script.js
@@ -1,54 +1,66 @@
 $(document).ready(function () {
-
-
-  $(".far.fa-caret-square-right").click(
-    function () {
+  function nextImg() {
     if ($(".uno").hasClass("active") == true) {
       $(".uno").removeClass("active");
       $(".uno").next().addClass("active");
     } else if ($(".due").hasClass("active") == true) {
       $(".due").removeClass("active");
       $(".due").next().addClass("active");
-     } else if ($(".tre").hasClass("active") == true) {
+    } else if ($(".tre").hasClass("active") == true) {
       $(".tre").removeClass("active");
       $(".tre").next().addClass("active");
-     } else if ($(".quattro").hasClass("active") == true) {
+    } else if ($(".quattro").hasClass("active") == true) {
       $(".quattro").removeClass("active");
       $(".uno").addClass("active");
+    }
+  }
+
+  $(".far.fa-caret-square-right").click(
+    // function nextImg() {
+    //   if ($(".uno").hasClass("active") == true) {
+    //     $(".uno").removeClass("active");
+    //     $(".uno").next().addClass("active");
+    //   } else if ($(".due").hasClass("active") == true) {
+    //     $(".due").removeClass("active");
+    //     $(".due").next().addClass("active");
+    //   } else if ($(".tre").hasClass("active") == true) {
+    //     $(".tre").removeClass("active");
+    //     $(".tre").next().addClass("active");
+    //   } else if ($(".quattro").hasClass("active") == true) {
+    //     $(".quattro").removeClass("active");
+    //     $(".uno").addClass("active");
+    //   }
+    // }
+  )
+
+  $(document).keydown (
+    function() {
+      if (event.which == 39) {
+        nextImg();
       }
     }
   )
-
-  $(".far.fa-caret-square-right").keydown(
-    function(){
-      $("i").css("background-color", "yellow");
-    }
-  );
 
 
 
 
   $(".far.fa-caret-square-left").click(
     function () {
-    if ($(".uno").hasClass("active") == true) {
-      $(".uno").removeClass("active");
-      $(".quattro").addClass("active");
-    } else if ($(".quattro").hasClass("active") == true) {
-      $(".quattro").removeClass("active");
-      $(".quattro").prev().addClass("active");
-     } else if ($(".tre").hasClass("active") == true) {
-      $(".tre").removeClass("active");
-      $(".tre").prev().addClass("active");
-    } else if ($(".due").hasClass("active") == true) {
-      $(".due").removeClass("active");
-      $(".due").prev().addClass("active");
+      if ($(".uno").hasClass("active") == true) {
+        $(".uno").removeClass("active");
+        $(".quattro").addClass("active");
+      } else if ($(".quattro").hasClass("active") == true) {
+        $(".quattro").removeClass("active");
+        $(".quattro").prev().addClass("active");
+      } else if ($(".tre").hasClass("active") == true) {
+        $(".tre").removeClass("active");
+        $(".tre").prev().addClass("active");
+      } else if ($(".due").hasClass("active") == true) {
+        $(".due").removeClass("active");
+        $(".due").prev().addClass("active");
       }
     }
   )
-
-
-
-
 
 
 });
