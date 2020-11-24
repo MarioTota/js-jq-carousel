7a645ffbaf1308015e198cd46833cc9dc33309c9
Mario <mario.tota.mt@gmail.com>
2020-11-25 00:35:15 +0100

    commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+//------------- due funzioni generiche-------------------------------
   function nextImg() {
     if ($(".uno").hasClass("active") == true) {
       $(".uno").removeClass("active");
@@ -30,6 +31,7 @@ $(document).ready(function () {
       $(".due").prev().addClass("active");
     }
   }
+//------------------spostamento foto al click delle freccette---------
 
   $(".far.fa-caret-square-right").click(
     function nextImg() {
@@ -67,13 +69,90 @@ $(document).ready(function () {
       }
     }
   )
-
+//--------------------movimento con freccette------------------------
   $(document).keydown (
     function() {
       if (event.which == 39) {
         nextImg();
       } else if (event.which == 37) {
         prevImg();
+      }
+    }
+  )
+//-------------------Click dei pallini-------------------------------
+
+  $(".fas.fa-circle.uno").click(
+    function() {
+      if ($("img.uno, .fas.fa-circle.uno").hasClass("active") == true) {
+        $("img.due, .fas.fa-circle.due").removeClass("active");
+        $("img.tre, .fas.fa-circle.tre").removeClass("active");
+        $("img.quattro, .fas.fa-circle.quattro").removeClass("active");
+      } else if ($(".due, .fas.fa-circle.due").hasClass("active") == true) {
+        $("img.due, .fas.fa-circle.due").removeClass("active");
+        $("img.uno, .fas.fa-circle.uno").addClass("active");
+      } else if ($(".tre, .fas.fa-circle.tre").hasClass("active") == true) {
+        $("img.tre, .fas.fa-circle.tre").removeClass("active");
+        $("img.uno, .fas.fa-circle.uno").addClass("active");
+      } else if ($(".quattro, .fas.fa-circle.quattro").hasClass("active") == true) {
+        $("img.quattro, .fas.fa-circle.quattro").removeClass("active");
+        $("img.uno, .fas.fa-circle.uno").addClass("active");
+      }
+    }
+  )
+
+  $(".fas.fa-circle.due").click(
+    function() {
+      if ($("img.uno, .fas.fa-circle.uno").hasClass("active") == true) {
+        $("img.uno, .fas.fa-circle.uno").removeClass("active");
+        $("img.due, .fas.fa-circle.due").addClass("active");
+      } else if ($(".due, .fas.fa-circle.due").hasClass("active") == true) {
+        $("img.uno, .fas.fa-circle.uno").removeClass("active");
+        $("img.tre, .fas.fa-circle.tre").removeClass("active");
+        $("img.quattro, .fas.fa-circle.quattro").removeClass("active");
+      } else if ($(".tre, .fas.fa-circle.tre").hasClass("active") == true) {
+        $("img.tre, .fas.fa-circle.tre").removeClass("active");
+        $("img.due, .fas.fa-circle.due").addClass("active");
+      } else if ($(".quattro, .fas.fa-circle.quattro").hasClass("active") == true) {
+        $("img.quattro, .fas.fa-circle.quattro").removeClass("active");
+        $("img.due, .fas.fa-circle.due").addClass("active");
+      }
+    }
+  )
+
+  $(".fas.fa-circle.tre").click(
+    function() {
+      if ($("img.uno, .fas.fa-circle.uno").hasClass("active") == true) {
+        $("img.uno, .fas.fa-circle.uno").removeClass("active");
+        $("img.tre, .fas.fa-circle.tre").addClass("active");
+      } else if ($(".due, .fas.fa-circle.due").hasClass("active") == true) {
+        $("img.due, .fas.fa-circle.due").removeClass("active");
+        $("img.tre, .fas.fa-circle.tre").addClass("active");
+      } else if ($(".tre, .fas.fa-circle.tre").hasClass("active") == true) {
+        $("img.uno, .fas.fa-circle.uno").removeClass("active");
+        $("img.due, .fas.fa-circle.due").removeClass("active");
+        $("img.quattro, .fas.fa-circle.quattro").removeClass("active");
+      } else if ($(".quattro, .fas.fa-circle.quattro").hasClass("active") == true) {
+        $("img.quattro, .fas.fa-circle.quattro").removeClass("active");
+        $("img.tre, .fas.fa-circle.tre").addClass("active");
+      }
+    }
+  )
+
+  $(".fas.fa-circle.quattro").click(
+    function() {
+      if ($("img.uno, .fas.fa-circle.uno").hasClass("active") == true) {
+        $("img.uno, .fas.fa-circle.uno").removeClass("active");
+        $("img.quattro, .fas.fa-circle.quattro").addClass("active");
+      } else if ($(".due, .fas.fa-circle.due").hasClass("active") == true) {
+        $("img.due, .fas.fa-circle.due").removeClass("active");
+        $("img.quattro, .fas.fa-circle.quattro").addClass("active");
+      } else if ($(".tre, .fas.fa-circle.tre").hasClass("active") == true) {
+        $("img.tre, .fas.fa-circle.tre").removeClass("active");
+        $("img.quattro, .fas.fa-circle.quattro").addClass("active");
+      } else if ($(".quattro, .fas.fa-circle.quattro").hasClass("active") == true) {
+        $("img.uno, .fas.fa-circle.uno").removeClass("active");
+        $("img.due, .fas.fa-circle.due").removeClass("active");
+        $("img.tre, .fas.fa-circle.tre").removeClass("active");
       }
     }
   )
